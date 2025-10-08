ddb23c5827d43ae639f6ebccf8c671e81e2137d5
Debanjan-Datta-VT <ddatta@vt.edu>
2025-10-08 15:25:31 +0000

    Deploying to gh-pages from @ Debanjan-Datta-VT/Debanjan-Datta-VT.github.io@e5102221e098c7e91d02f1be444c981730e062f2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -113,7 +113,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%64%64%61%74%74%61@%76%74.%65%64%75", "_blank");
         },
       },{
         id: 'social-linkedin',

568121a7573f602aed70b2a2dc9e835f47cbd325
Debanjan-Datta-VT <ddatta@vt.edu>
2025-12-04 04:13:28 +0000

    Deploying to gh-pages from @ Debanjan-Datta-VT/Debanjan-Datta-VT.github.io@82cc3c6efa0089eb7e85754cbb628206ccab450d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -66,9 +66,9 @@ ninja.data = [{
           description: "AWS Sagemaker Canvas Enterprise Project Roadmap",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-time-series-anomaly-detection",
+          title: 'Time Series Anomaly Detection',
+          description: "AWS Lookout for Metrics Service Enhancement",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
